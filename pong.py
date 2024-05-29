e050c28203ff962428b4d04c07a98c6e48fe651b
zarinzaidi <aereinzayed@yahoo.com>
2024-05-29 17:07:26 +0800

    Sound updated

--- pong.py
+++ pong.py
@@ -1,5 +1,6 @@
-# using turtle module easier for beginner. no need to install like pygame.
-import turtle
+import turtle #using turtle module easier for beginner. no need to install like pygame.
+import winsound #to use sound
+# import os #to use sound on Linux/Mac
 
 # setup for the game windows
 wn = turtle.Screen()
@@ -29,7 +30,7 @@ paddle_b.shape('square') # by default shape is 25px x 25px
 paddle_b.color('white')
 paddle_b.shapesize(stretch_wid=5, stretch_len= 1) #stretch the paddle size
 paddle_b.penup()
-paddle_b.goto(+350, 0) #paddle a starts at coordinate (x, y)
+paddle_b.goto(+350, 0) #paddle b starts at coordinate (x, y)
 
 # Ball
 ball = turtle.Turtle() #turtle object. Class name: Turtle
@@ -39,8 +40,8 @@ ball.color('white')
 ball.penup()
 ball.goto(0, 0) #ball a starts at coordinate (x, y)
 # separate the ball movements into parts (x move & y move)
-ball.dx = 0.1 #ball move by 2px on x-axis
-ball.dy = 0.1 #ball move by 2px on y-axis
+ball.dx = 0.1 #ball move by 0.1px on x-axis
+ball.dy = 0.1 #ball move by 0.1px on y-axis
 
 # Pen
 pen = turtle.Turtle() #turtle object. Class name: Turtle
@@ -73,12 +74,17 @@ def paddle_b_down():
     y -= 25 #adds 20px to y coor
     paddle_b.sety(y)
     
+# Update the score display
+def update_score():
+    pen.clear()  # Clear the pen first so the score won't overlap
+    pen.write('Player A: {}  Player B: {}'.format(score_a, score_b), align='center', font=('Courier', 15, 'normal'))  # Display updated score
+    
 # Keyboard binding
 wn.listen() #listen for keyboard input
-wn.onkeypress(paddle_a_up, 'w') #call the fx when user press 'w'
-wn.onkeypress(paddle_a_down, 's') #call the fx when user press 'w'
-wn.onkeypress(paddle_b_up, 'Up') #call the fx when user press 'w'
-wn.onkeypress(paddle_b_down, 'Down') #call the fx when user press 'w'
+wn.onkeypress(paddle_a_up, 'a') #call the fx when user press 'a'
+wn.onkeypress(paddle_a_down, 'z') #call the fx when user press 'z'
+wn.onkeypress(paddle_b_up, 'Up') #call the fx when user press 'Up' arrow key
+wn.onkeypress(paddle_b_down, 'Down') #call the fx when user press 'Down' arrow key
 
 
 # Main game loop
@@ -94,19 +100,26 @@ while True:
     if ball.ycor() > 290: #windows is y=300 and ball is 20/2=10
         ball.sety(290) #set it back to 290
         ball.dy *= -1 #to reverse the ball's direction
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC) #play sound
+        # os.system('afplay bounce.wav) #play sound for Mac
+        # os.system('aplay bounce.wav) #play sound for Linux
+        
         
     # bottom border
     if ball.ycor() < -290: #windows is y=600/2=300 and ball is 20/2=10
         ball.sety(-290) #set it back to 290
         ball.dy *= -1 #to reverse the ball's direction
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC) #play sound
+        # os.system('afplay bounce.wav) #play sound for Mac
+        # os.system('aplay bounce.wav) #play sound for Linux
+        
         
     # right border
     if ball.xcor() > 390: #windows is x=800/2=400 and ball is 20/2=10
         ball.goto(0, 0) #put the ball back to center
         ball.dx *= -1 #reverse the ball's direction
         score_a +=1 #player A(left) got 1 score if the ball goes off the right side
-        pen.clear() #clear the pen first so the score won't overlap
-        pen.write('Player A: {}  Player B: {}'.format(score_a, score_b), align='center', font=('Courier', 15, 'normal')) #Display updated score
+        update_score()
         
         
     # left border
@@ -114,19 +127,25 @@ while True:
         ball.goto(0, 0) #put the ball back to center
         ball.dx *= -1 #reverse the ball's direction
         score_b +=1 #player B(right) got 1 score if the ball goes off the left side
-        pen.clear() #clear the pen first so the score won't overlap
-        pen.write('Player A: {}  Player B: {}'.format(score_a, score_b), align='center', font=('Courier', 15, 'normal')) #Display updated score
+        update_score
         
-        
+           
     # Paddle and ball collisions detection
     # right paddle detection
     # if the ball's and paddles' edges are touching AND is the ball between the top and bottom of the paddle(size=100/2=50 but minus ball size=20/2=10)
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40): 
         ball.setx(340) #set it back to 340
         ball.dx *= -1 #reverse and bounce back the ball
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC) #play sound
+        # os.system('afplay bounce.wav) #play sound for Mac
+        # os.system('aplay bounce.wav) #play sound for Linux
+        
         
     # left paddle detection
     # if the ball's and paddles' edges are touching AND is the ball between the top and bottom of the paddle(size=100/2=50 but minus ball size=20/2=10)
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40): 
         ball.setx(-340) #set it back to -340
         ball.dx *= -1 #reverse and bounce back the ball
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC) #play sound
+        # os.system('afplay bounce.wav) #play sound for Mac
+        # os.system('aplay bounce.wav) #play sound for Linux
