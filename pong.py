cff9d45e68231b50a72532019dbb8d2665bcd185
zarinzaidi <aereinzayed@yahoo.com>
2024-05-29 11:46:12 +0800

    Paddle comp setup

--- pong.py
+++ pong.py
@@ -9,3 +9,27 @@ wn.setup(width=800, height=600)
 wn.tracer(0) 
 # tracer stops the window from updating. so we hv to manually update it.speed up our game a bit.
 
+
+# Paddle A
+paddle_a = turtle.Turtle() #turtle object. Class name: Turtle
+paddle_a.speed(0) #speed of animation. sets the speed to the maxi possible speed
+paddle_a.shape('square') # by default shape is 25px x 25px
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len= 1) #stretch the paddle size
+paddle_a.penup()
+paddle_a.goto(-350, 0) #paddle a starts at coordinate (x, y)
+
+# Paddle B
+paddle_b = turtle.Turtle() #turtle object. Class name: Turtle
+paddle_b.speed(0) #speed of animation. sets the speed to the maxi possible speed
+paddle_b.shape('square') # by default shape is 25px x 25px
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid=5, stretch_len= 1) #stretch the paddle size
+paddle_b.penup()
+paddle_b.goto(+350, 0) #paddle a starts at coordinate (x, y)
+
+# Ball
+
+# Main game loop
+while True:
+    wn.update()
